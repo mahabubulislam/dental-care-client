9f4849fe95afa7c4d204519b29e841e1c65b9dcf
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-20 12:59:51 +0600

    services section added

---
+++ src/Pages/Home/Service.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Service = ({service}) => {
+    const {img, name, description} = service
+    return (
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl hover:scale-105 hover:bg-neutral hover:text-white  ease-in-out duration-500">
+        <figure className="px-10 pt-10">
+            <img src={img} alt="Shoes" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">{name}</h2>
+            <p>{description}</p>
+        </div>
+    </div>
+    );
+};
+
+export default Service;

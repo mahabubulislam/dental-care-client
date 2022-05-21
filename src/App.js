ce2b570971c91026fb010dae174d683f4948ac34
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-21 14:40:37 +0600

    firebase signin added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./Pages/About/About";
 import Appoinment from "./Pages/Appoinment/Appoinment";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Navbar from "./Pages/Shared/Navbar";
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="appoinment" element={<Appoinment/>}></Route>
+        <Route path="login" element={<Login/>}></Route>
       </Routes>
     </div>
   );
